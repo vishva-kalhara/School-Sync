667841a9191e6e954bd052461ae550762099996e
Wishva Kalhara <vishvakalhara@gmail.com>
2024-10-22 22:01:07 +0530

    env

---
+++ src/utils/AppConnection.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException; 
+
+/**
+ *
+ * @author vishv
+ */
+public class AppConnection {
+    
+    private static Connection connection;
+
+    private static void setUpConnection() {
+
+        if (connection == null) {
+            try {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/members_v2", "root", "Wishva!23");
+
+            } catch (SQLException | ClassNotFoundException e) {
+//                FrmSplashScreen.logger.log(Level.WARNING, e.getMessage() ,e);
+            }
+        }
+    }
+
+    public static ResultSet fetch(String query) {
+
+        if (!query.startsWith("SELECT") && !query.startsWith("CALL")) {
+            throw new IllegalArgumentException("Use ResultSet utils.AppConnection.mutate()");
+        }
+
+        if (connection == null) {
+            setUpConnection();
+        }
+
+        try {
+            return connection.createStatement().executeQuery(query);
+        } catch (SQLException e) {
+//           FrmSplashScreen.logger.log(Level.WARNING, e.getMessage() ,e);
+//            new DlgError(AppLayout.appLayout, true, e.getMessage()).setVisible(true);
+            return null;
+        }
+    }
+
+    public static boolean mutate(String query) throws SQLException {
+
+        if (query.startsWith("SELECT")) {
+            throw new IllegalArgumentException("Use ResultSet utils.AppConnection.fetch()");
+        }
+
+        if (connection == null) {
+            setUpConnection();
+        }
+
+        int result = connection.createStatement().executeUpdate(query);
+
+        return result == 1;
+    }
+
+    public static void closeConnection() throws SQLException {
+            
+        if(connection != null){
+            connection.close();
+        }
+
+    }
+}
