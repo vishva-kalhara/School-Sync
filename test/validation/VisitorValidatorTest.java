88a66e39f539bb735dfbcf889b9b9f01dd9b89c0
unknown <dinithiwm@gmail.com>
2024-12-09 21:24:12 +0530

    Visitor Validation

---
+++ test/validation/VisitorValidatorTest.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package validation;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Dini
+ */
+public class VisitorValidatorTest {
+
+    //happy path
+    // student id cannot be 0
+    //date cannot be null
+    //date must be a future
+}
