d1b517aeccfa688bac426bcc2e785cdf89fba97a
Chamod-Fernando <chamodsanjula59@gmail.com>
2025-05-08 19:55:58 +0530

    completed-appoinment-sec

--- src/views/internals/PnlAppointments.java
+++ src/views/internals/PnlAppointments.java
@@ -6,10 +6,19 @@ package views.internals;
 
 import com.formdev.flatlaf.FlatClientProperties;
 import enums.LayoutPage;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Vector;
+import java.util.logging.Level;
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JLabel;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
+import utils.AppConnection;
 import views.dialogs.DlgAppointment;
+import views.dialogs.DlgError;
+import views.forms.FrmSplashScreen;
 import views.layouts.AppLayout;
 
 /**
@@ -18,16 +27,19 @@ import views.layouts.AppLayout;
  */
 public class PnlAppointments extends javax.swing.JPanel {
 
+    private final HashMap<String, Integer> classesMap = new HashMap<>();
+
     /**
      * Creates new form PnlAppointments
      */
     public PnlAppointments() {
         initComponents();
-        
+        loadClasses();
+        loadTableData("");
         setDsign();
     }
-    
-     private void setDsign() {
+
+    private void setDsign() {
         btnAdd.putClientProperty("JButton.buttonType", "borderless");
         btnReport.putClientProperty("JButton.buttonType", "borderless");
         btnPrint.putClientProperty("JButton.buttonType", "borderless");
@@ -49,6 +61,127 @@ public class PnlAppointments extends javax.swing.JPanel {
         scroll.setBorder(BorderFactory.createEmptyBorder());
     }
 
+    private void loadClasses() {
+        try {
+            ResultSet rs = AppConnection.search("SELECT grades_has_classes.id AS Id, "
+                    + "CONCAT(grades.value, '-', grades_has_classes.class) AS grade_class "
+                    + "FROM school_sync_v1.grades_has_classes INNER JOIN grades "
+                    + "ON grades_has_classes.grades_id = grades.id "
+                    + "ORDER BY grades.value ASC, grades_has_classes.class ASC");
+
+            Vector<String> data = new Vector();
+            data.add("Select");
+
+            while (rs.next()) {
+                classesMap.put(rs.getString("grade_class"), rs.getInt("id"));
+                data.add(rs.getString("grade_class"));
+            }
+
+            DefaultComboBoxModel model = new DefaultComboBoxModel(data);
+            cboClass.setModel(model);
+
+        } catch (Exception e) {
+            FrmSplashScreen.logger.log(Level.WARNING, e.getMessage(), e);
+        }
+    }
+
+    private void loadTableData(String constraints) {
+        try {
+            DefaultTableModel model = (DefaultTableModel) table.getModel();
+            model.setRowCount(0);
+
+            ResultSet rs = AppConnection.search("SELECT "
+                    + "appointments.created_at AS created_at, "
+                    + "appointments.student_id AS stuid, "
+                    + "student.full_name AS stuname, "
+                    + "appointments.title AS appointment, "
+                    + "CONCAT"
+                    + "(grades_has_classes.grades_id, ' - ', grades_has_classes.class) AS class "
+                    + "FROM appointments INNER JOIN student ON "
+                    + "appointments.student_id = student.id "
+                    + "INNER JOIN grades_has_classes ON "
+                    + "appointments.grades_has_classes_id = grades_has_classes.id "
+                    + "INNER JOIN users ON appointments.users_id = users.id "
+                    + "INNER JOIN grades ON grades_has_classes.grades_id = grades.id"
+                    + constraints
+                    + ";"
+            );
+
+            while (rs.next()) {
+                Vector<String> data = new Vector<>();
+                data.add(rs.getString("created_at"));
+                data.add(rs.getString("stuid"));
+                data.add(rs.getString("stuname"));
+                data.add(rs.getString("class"));
+                data.add(rs.getString("appointment"));
+
+                model.addRow(data);
+            }
+
+            if (model.getRowCount() == 0) {
+                jScrollPane1.setViewportView(new PnlNotFound());
+            } else {
+                jScrollPane1.setViewportView(this.table);
+                btnPrint.setEnabled(true);
+                btnReport.setEnabled(true);
+            }
+
+            btnRefresh.setEnabled(true);
+            btnClearFilters.setEnabled(true);
+        } catch (Exception e) {
+            new DlgError(AppLayout.appLayout, true, e.getMessage()).setVisible(true);
+            FrmSplashScreen.logger.log(Level.WARNING, e.getMessage(), e);
+        }
+
+    }
+
+    private void fetchData(String constraints) {
+
+        jScrollPane1.setViewportView(new PnlFetching());
+        btnPrint.setEnabled(false);
+        btnReport.setEnabled(false);
+        btnRefresh.setEnabled(false);
+        btnClearFilters.setEnabled(false);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                loadTableData(constraints);
+            }
+        }).start();
+    }
+
+    private void filterData() {
+
+        StringBuilder constraints = new StringBuilder("");
+
+        boolean hasSearch = !txtSearch.getText().isBlank();
+        boolean hasClass = cboClass.getSelectedIndex() != 0;
+
+        if (hasClass || hasSearch) {
+            constraints.append(" WHERE ");
+        }
+        if (hasSearch) {
+            constraints
+                    .append(" student.full_name LIKE '%")
+                    .append(txtSearch.getText())
+                    .append("%' ");
+        }
+        if (hasClass) {
+
+            if (hasSearch) {
+                constraints.append(" AND ");
+            }
+
+            constraints
+                    .append(" appointments.grades_has_classes_id = '")
+                    .append(classesMap.get(String.valueOf(cboClass.getSelectedItem())))
+                    .append("' ");
+        }
+
+        fetchData(constraints.toString());
+    }
+
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -67,7 +200,6 @@ public class PnlAppointments extends javax.swing.JPanel {
         btnAccount = new javax.swing.JButton();
         btnClearFilters = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
-        cboSort = new javax.swing.JComboBox<>();
         txtSearch = new javax.swing.JTextField();
         pnlTable = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
@@ -92,6 +224,11 @@ public class PnlAppointments extends javax.swing.JPanel {
         btnPrint.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/icons/printer.png"))); // NOI18N
 
         btnRefresh.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/icons/refresh-cw.png"))); // NOI18N
+        btnRefresh.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnRefreshActionPerformed(evt);
+            }
+        });
 
         btnLogout.setIcon(new javax.swing.ImageIcon(getClass().getResource("/assets/icons/log-out.png"))); // NOI18N
         btnLogout.addActionListener(new java.awt.event.ActionListener() {
@@ -149,13 +286,6 @@ public class PnlAppointments extends javax.swing.JPanel {
         add(jPanel1, java.awt.BorderLayout.EAST);
 
         jPanel2.setBackground(new java.awt.Color(247, 247, 247));
-
-        cboSort.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "All Statuses", "Attented", "Not Attented" }));
-        cboSort.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cboSortActionPerformed(evt);
-            }
-        });
 
         txtSearch.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -216,22 +346,20 @@ public class PnlAppointments extends javax.swing.JPanel {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
                 .addGap(26, 26, 26)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(pnlTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addGroup(jPanel2Layout.createSequentialGroup()
-                        .addComponent(cboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(cboSort, javax.swing.GroupLayout.PREFERRED_SIZE, 173, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addComponent(pnlTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGap(24, 24, 24))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addGap(38, 38, 38)
+                .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(cboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(24, 24, 24)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cboSort, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(cboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(18, 18, 18)
@@ -243,7 +371,7 @@ public class PnlAppointments extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnAddActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddActionPerformed
-        
+
         new DlgAppointment(AppLayout.appLayout, true).setVisible(true);
     }//GEN-LAST:event_btnAddActionPerformed
 
@@ -252,22 +380,22 @@ public class PnlAppointments extends javax.swing.JPanel {
         AppLayout.appLayout.closeApp();
     }//GEN-LAST:event_btnLogoutActionPerformed
 
-    private void cboSortActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboSortActionPerformed
-
-    }//GEN-LAST:event_cboSortActionPerformed
-
     private void txtSearchActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSearchActionPerformed
-
+        filterData();
     }//GEN-LAST:event_txtSearchActionPerformed
 
     private void cboClassActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cboClassActionPerformed
-
+        filterData();
     }//GEN-LAST:event_cboClassActionPerformed
 
     private void btnClearFiltersActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnClearFiltersActionPerformed
-        
+
         AppLayout.appLayout.changeForm(LayoutPage.APPOINTMENTS);
     }//GEN-LAST:event_btnClearFiltersActionPerformed
+
+    private void btnRefreshActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshActionPerformed
+        loadTableData("");
+    }//GEN-LAST:event_btnRefreshActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
@@ -279,7 +407,6 @@ public class PnlAppointments extends javax.swing.JPanel {
     private javax.swing.JButton btnRefresh;
     private javax.swing.JButton btnReport;
     private javax.swing.JComboBox<String> cboClass;
-    private javax.swing.JComboBox<String> cboSort;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
