c1d5298827e6bb004312541394fde4f171006be0
vishva-kalhara <vishvakalhara@gmail.com>
2025-05-09 08:24:56 +0530

    appointments table bug fixed

--- src/views/internals/PnlAppointments.java
+++ src/views/internals/PnlAppointments.java
@@ -47,8 +47,8 @@ public class PnlAppointments extends javax.swing.JPanel {
     public PnlAppointments() {
         initComponents();
         loadClasses();
-        loadTableData("");
         setDsign();
+        loadTableData("");
     }
 
     private void setDsign() {
@@ -132,10 +132,10 @@ public class PnlAppointments extends javax.swing.JPanel {
 
             if (model.getRowCount() == 0) {
                 jScrollPane1.setViewportView(new PnlNotFound());
+                btnPrint.setEnabled(false);
+                btnReport.setEnabled(false);
             } else {
                 jScrollPane1.setViewportView(this.table);
-                btnPrint.setEnabled(true);
-                btnReport.setEnabled(true);
             }
 
             btnRefresh.setEnabled(true);
@@ -371,16 +371,15 @@ public class PnlAppointments extends javax.swing.JPanel {
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+            .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(26, 26, 26)
-                .addComponent(pnlTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(cboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(pnlTable, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addGap(24, 24, 24))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
-                .addGap(38, 38, 38)
-                .addComponent(txtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, 343, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(cboClass, javax.swing.GroupLayout.PREFERRED_SIZE, 151, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(32, 32, 32))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
